9387b83f542a4464e38fa1f27acdedb4774e8a10
dsasi86 <95671014+dsasi86@users.noreply.github.com>
2021-12-28 13:47:29 +0530

    Update puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -11,7 +11,7 @@ threads min_threads_count, max_threads_count
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
 #
-worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "production") == "production"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
@@ -38,6 +38,8 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # process behavior so workers use less memory.
 #
  preload_app!
-
+ on_worker_boot do
+    ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
+ end
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
